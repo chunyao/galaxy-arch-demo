a13c1039c7bc2212376deb2e8e19ffc4ca6c899c
淳遥 <chunyao@mabangerp.com>
2022-07-05 23:18:08 +0800

    run

---
+++ mabang-arch-demo/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$1" = "dev" ];
+then
+  echo "dev"
+  php app.php --env=dev --url=https://dev-nacos.mabangerp.com  --dataId=mico_service --group=V2SYSTEM_GROUP
+fi
+
+if [ "$1" = "test" ];
+then
+  echo "test"
+  php app.php --env=test --url=https://dev-nacos.mabangerp.com  --dataId=mico_service --group=V2SYSTEM_GROUP
+fi
+
+if [ "$1" = "prod" ];
+then
+  echo "prod"
+  php app.php --env=prod --url=https://dev-nacos.mabangerp.com --dataId=mico_service --group=V2SYSTEM_GROUP
+fi
