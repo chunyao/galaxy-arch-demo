8ebddbfc5d1ea33f3b1fd0475ff75c32345126d7
淳遥 <chunyao@mabangerp.com>
2022-07-06 22:49:54 +0800

    t

--- test.php
+++ test.php
@@ -5,3 +5,4 @@ SeasLog::setLogger("/mabang-arch-service");
 $options = ["connect" => true];
 $result =  new MongoDB\Driver\Manager('mongodb://jiagou@192.168.2.20:27017,192.168.2.21:27017/?replicaSet=mongos&authSource=admin', $options);
 var_dump($result);
+echo "123";
