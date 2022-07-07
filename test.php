baaf73c5fb87ddc22de8a8918a78c71e1d2ea705
淳遥 <chunyao@mabangerp.com>
2022-07-07 21:05:16 +0800

    mongoDB 数据库数据读取

--- test.php
+++ test.php
@@ -1,15 +1,10 @@
 <?php
 SeasLog::setBasePath("./data/logs");
 SeasLog::setLogger("/mabang-arch-service");
-
-$options = ["connect" => true];
-$manager =  new MongoDB\Driver\Manager('mongodb://jiagou:mtViTGogNLH2iwg@192.168.2.20:27017,192.168.2.21:27017/?replicaSet=mongos&authSource=admin',$options);
-var_dump($manager);
-$manager->
-$filter = ['appId' => ['$gt' => 1]];
-$options = [
-    'sort' => ['x' => -1],
-];
-$query = new MongoDB\Driver\Query($filter);
-$cursor = $manager->executeQuery('mdc_product_online.tb_product', $query);
-
+require "./vendor/autoload.php";
+use Elasticsearch\ClientBuilder;
+$client = ClientBuilder::create()
+    ->setHosts(['192.168.2.45:9200'])
+    ->build();
+$response = $client->info();
+var_dump($response);
