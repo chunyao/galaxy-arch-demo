4c4a735b5ca252374c6b3781d5ac580eca63ce05
淳遥 <chunyao@mabangerp.com>
2022-07-08 14:52:05 +0800

    es

--- service-main/src/App/Service/WishbrandService.php
+++ service-main/src/App/Service/WishbrandService.php
@@ -5,16 +5,17 @@ namespace App\Service;
 
 use App\Config\Suffix;
 use App\Repository\Model\WishbrandModel;
-
+use  Galaxy\Common\Utils\SnowFlakeUtils;
+use App\Repository\Model\ES\Test;
 class WishbrandService
 {
     private WishbrandModel $wishbrandModel;
-
+    private Test $es;
     public function __construct()
     {
 
         $this->wishbrandModel = new WishbrandModel();
-
+        $this->es = new Test();
     }
 
     public function findById(int $id)
@@ -23,5 +24,18 @@ class WishbrandService
         $data = $this->wishbrandModel->findById($id);
 
         return $data;
+    }
+    public function insertEs($data)
+    {
+        $Esid = null;
+        try {
+            $Esid = SnowFlakeUtils::generateID();
+        }catch (\Exception $e){
+
+        }
+
+        $return = $this->es->insert($data,$Esid);
+
+        return $return;
     }
 }
