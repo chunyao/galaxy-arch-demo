65a7634fd82b5e5a03fb2400a695278610e0b5b3
淳遥 <chunyao@mabangerp.com>
2022-08-04 18:06:01 +0800

    app 时区

--- run.sh
+++ run.sh
@@ -2,17 +2,23 @@
 if [ "$1" = "dev" ];
 then
   echo "dev"
-  php app.php --env=dev --url=https://dev-nacos.mabangerp.com  --dataId=mico_service --group=V2SYSTEM_GROUP --server.port=8080 --management.server.port=8081 --log.path=/data/logs
+  php app.php --env=test --url=https://dev-nacos.mabangerp.com/nacos/v1/cs/configs  --dataId=mas_shopee_micro_queue_data_id --group=dev_erp_group --management.server.port=8081 --tenant=eb133ea5-2987-47be-beac-03022efd6798 --log.path=/data/logs
 fi
 
 if [ "$1" = "test" ];
 then
   echo "test"
-  php app.php --env=test --url=https://dev-nacos.mabangerp.com  --dataId=mico_service --group=V2SYSTEM_GROUP --log.path=/data/logs
+  php app.php --env=test --url=https://dev-nacos.mabangerp.com/nacos/v1/cs/configs  --dataId=mas_shopee_micro_queue_data_id --group=dev_erp_group --management.server.port=8081  --tenant=eb133ea5-2987-47be-beac-03022efd6798 --log.path=/data/logs
 fi
 
-if [ "$1" = "prod" ];
+if [ "$1" = "public" ];
 then
-  echo "prod"
-  php app.php --env=prod --url=https://dev-nacos.mabangerp.com --dataId=mico_service --group=V2SYSTEM_GROUP --log.path=/data/logs
+  echo "prd"
+  php app.php --env=prd --url=https://nacos.mabangerp.com/nacos/v1/cs/configs --dataId=mas_shopee_micro_queue_data_id --group=prd_erp_group --management.server.port=8081  --tenant=ae6ba4f8-dc91-42fb-b79c-4c4f7bb01482 --log.path=/data/logs
+fi
+
+if [ "$1" = "private" ];
+then
+  echo "prd"
+  php app.php --env=prd --url=https://nacos.mabangerp.com/nacos/v1/cs/configs --dataId=mas_shopee_micro_queue_data_id --group=prd_erp_group --management.server.port=8081  --tenant=a899e072-8d9c-411f-be51-cb6a467b868c --log.path=/data/logs
 fi
