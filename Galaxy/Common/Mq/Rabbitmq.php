3b3ec3a82e5dd275f7df4698f215b2d3821695dc
淳遥 <chunyao@mabangerp.com>
2022-09-13 11:07:49 +0800

    异常

--- Galaxy/Common/Mq/Rabbitmq.php
+++ Galaxy/Common/Mq/Rabbitmq.php
@@ -47,36 +47,55 @@ class Rabbitmq
      */
     public function publish($messageBody, $exchange, $routeKey, $head = [],$ack=0)
     {
+        $status=0;
+        static $i = 0;
         if (empty($this->channel)) {
             $this->con = new AMQPStreamConnection($this->host, $this->port, $this->username, $this->password, $this->vhost, false, 'AMQPLAIN', null, 'en_US', 3, 21, null, false, 10);
         }
-        $this->channel = $this->con->channel(rand(0,1000));
-        $head = array_merge(array('content_type' => 'text/json', 'content_encoding' => 'gzip', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT), $head);
-        $message = new AMQPMessage($messageBody, $head);
-        //推送成功
-        $status=0;
-        if ($ack===1) {
-            $this->channel->set_ack_handler(
-                function (AMQPMessage $message) use (&$status) {
-                    $status = 1;
-                    //  echo "发送成功: " . $message->body . PHP_EOL;
-                }
-            );
+        try{
+            $this->channel = $this->con->channel(rand(0,1000));
+            $head = array_merge(array('content_type' => 'text/json', 'content_encoding' => 'gzip', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT), $head);
+            $message = new AMQPMessage($messageBody, $head);
+            //推送成功
 
-            //推送失败
-            $this->channel->set_nack_handler(
-                function (AMQPMessage $message) use (&$status) {
-                    $status = 2;
-                    //   echo "发送失败: " . $message->body . PHP_EOL;
+            if ($ack===1) {
+                $this->channel->set_ack_handler(
+                    function (AMQPMessage $message) use (&$status) {
+                        $status = 1;
+                        //  echo "发送成功: " . $message->body . PHP_EOL;
+                    }
+                );
+
+                //推送失败
+                $this->channel->set_nack_handler(
+                    function (AMQPMessage $message) use (&$status) {
+                        $status = 2;
+                        //   echo "发送失败: " . $message->body . PHP_EOL;
+                    }
+                );
+                $this->channel->confirm_select();
+            }
+
+            $this->channel->basic_publish($message, $exchange, $routeKey);
+            if ($ack===1){
+                $this->channel->wait_for_pending_acks();
+                $this->channel->close();
+            }
+        }catch (\Throwable $e){
+            $this->con = new AMQPStreamConnection($this->host, $this->port, $this->username, $this->password, $this->vhost, false, 'AMQPLAIN', null, 'en_US', 3, 21, null, false, 10);
+            swoole_timer_tick(10000, function ()  {
+                try {
+                    $this->con->checkHeartBeat();
+                } catch (\Throwable $e) {
+                    //var_dump($e);
                 }
-            );
+            });
+            if($i<3){
+                $this->publish($messageBody, $exchange, $routeKey, $head = [],$ack=0);
+            }
         }
-        $this->channel->confirm_select();
-        $this->channel->basic_publish($message, $exchange, $routeKey);
-        if ($ack===1){
-        $this->channel->wait_for_pending_acks();
-        $this->channel->close();
-        }
+
+
         // 响应ack
 
         return $status;
